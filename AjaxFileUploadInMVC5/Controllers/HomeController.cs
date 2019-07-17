085872ea44ca1cb6b247ffa8ac2b0c620c30671a
Mahesh Rupapara <mahesh.rupapara@gmail.com>
2019-07-17 12:52:54 +0530

    Initial Commit

---
+++ AjaxFileUploadInMVC5/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AjaxFileUploadInMVC5.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Uploads()
+        {
+            if (Request.Files.Count > 0)
+            {
+                try
+                {
+                    HttpFileCollectionBase files = Request.Files;
+                    for (int i = 0; i < files.Count; i++)
+                    {
+                        HttpPostedFileBase file = files[i];
+                        string fname;
+                        if (Request.Browser.Browser.ToUpper() == "IE" || Request.Browser.Browser.ToUpper() == "INTERNETEXPLORER")
+                        {
+                            string[] testfiles = file.FileName.Split(new char[] { '\\' });
+                            fname = testfiles[testfiles.Length - 1];
+                        }
+                        else
+                        {
+                            fname = file.FileName;
+                        }
+                        var uploadRootFolderInput = AppDomain.CurrentDomain.BaseDirectory + "\\Uploads";
+                        Directory.CreateDirectory(uploadRootFolderInput);
+                        var directoryFullPathInput = uploadRootFolderInput;
+                        fname = Path.Combine(directoryFullPathInput, fname);
+                        file.SaveAs(fname);
+                    }
+                    return Json("File Uploaded Successfully!");
+                }
+                catch (Exception ex)
+                {
+                    return Json("Error occurred. Error details: " + ex.Message);
+                }
+            }
+            else
+            {
+                return Json("No files selected.");
+            }
+        }
+    }
+}
---
+++ AjaxFileUploadInMVC5/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AjaxFileUploadInMVC5.MvcApplication" Language="C#" %>
